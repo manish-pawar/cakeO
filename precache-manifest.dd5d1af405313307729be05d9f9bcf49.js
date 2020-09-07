7b143a70ee3ffc122dd672a4597757a9be07dd5d
manish-pawar <pawarmanish99999@gmail.com>
2020-09-07 11:54:58 +0530

    Updates

---
+++ precache-manifest.dd5d1af405313307729be05d9f9bcf49.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73379915074484952fb81ac6670a366",
+    "url": "/cakeO/index.html"
+  },
+  {
+    "revision": "c78b19391d01a08edc77",
+    "url": "/cakeO/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "82c33e7cf18d51a123ee",
+    "url": "/cakeO/static/css/main.741234e8.chunk.css"
+  },
+  {
+    "revision": "c78b19391d01a08edc77",
+    "url": "/cakeO/static/js/2.ca9ddc53.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/cakeO/static/js/2.ca9ddc53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c33e7cf18d51a123ee",
+    "url": "/cakeO/static/js/main.6b05484f.chunk.js"
+  },
+  {
+    "revision": "bbf7470d1591208f67bf",
+    "url": "/cakeO/static/js/runtime-main.c3be28bc.js"
+  },
+  {
+    "revision": "9225c7e0bf6bd4ed6541be94ab9a6c28",
+    "url": "/cakeO/static/media/c1.9225c7e0.jpg"
+  },
+  {
+    "revision": "f4bbe7c562946fad65e8d679c383943d",
+    "url": "/cakeO/static/media/c10.f4bbe7c5.jpg"
+  },
+  {
+    "revision": "f03331b524d9e9730fc7ac78602b8202",
+    "url": "/cakeO/static/media/c11.f03331b5.jpg"
+  },
+  {
+    "revision": "acd9c1b52fe5d9160595c653d20e8751",
+    "url": "/cakeO/static/media/c12.acd9c1b5.jpg"
+  },
+  {
+    "revision": "6447b3babd7daf5412e377b0e9730003",
+    "url": "/cakeO/static/media/c13.6447b3ba.jpg"
+  },
+  {
+    "revision": "8edd9bed78727bdb565f71ab6b986cb8",
+    "url": "/cakeO/static/media/c2.8edd9bed.jpg"
+  },
+  {
+    "revision": "51de5877653363ddfdde334e4aa021f4",
+    "url": "/cakeO/static/media/c3.51de5877.jpg"
+  },
+  {
+    "revision": "34764433d5d863239a81e2ccead805bf",
+    "url": "/cakeO/static/media/c4.34764433.jpg"
+  },
+  {
+    "revision": "b90050a160f7e6a8873ccfe9c9170033",
+    "url": "/cakeO/static/media/c5.b90050a1.jpg"
+  },
+  {
+    "revision": "21c27bb0b09360a96e0963e33af15762",
+    "url": "/cakeO/static/media/c6.21c27bb0.jpg"
+  },
+  {
+    "revision": "e01d3ef9ca2b4a72596d5f7f0f5cc30e",
+    "url": "/cakeO/static/media/c7.e01d3ef9.jpg"
+  },
+  {
+    "revision": "add5dd8c3d69c4d052f0db38bb564518",
+    "url": "/cakeO/static/media/c8.add5dd8c.jpg"
+  },
+  {
+    "revision": "6952c34728298507a627a3b45d27b04f",
+    "url": "/cakeO/static/media/de.6952c347.jpg"
+  },
+  {
+    "revision": "03a91e2fa9b9a3ddd876abb5dfbaebb9",
+    "url": "/cakeO/static/media/giveaway.03a91e2f.png"
+  },
+  {
+    "revision": "391ea7505f294d90d6cfab121527af68",
+    "url": "/cakeO/static/media/off.391ea750.jpg"
+  },
+  {
+    "revision": "afc67db80376b244dcb57e5e1925b1e7",
+    "url": "/cakeO/static/media/sc.afc67db8.jpg"
+  },
+  {
+    "revision": "0146ab12bbad038b3f0c12c3af8796bb",
+    "url": "/cakeO/static/media/ser.0146ab12.jpg"
+  },
+  {
+    "revision": "ffce398dc1404836be56e14f20ea6dcb",
+    "url": "/cakeO/static/media/sm.ffce398d.jpg"
+  },
+  {
+    "revision": "db182386d4e85e59c08f44b8d4690b77",
+    "url": "/cakeO/static/media/we.db182386.jpg"
+  }
+]);
